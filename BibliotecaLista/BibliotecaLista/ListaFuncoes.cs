4a4c71c2a2085719f885209de7df4d6216d6f823
Lucas Silva de Faria <87736324+LucasTI23@users.noreply.github.com>
2022-06-24 21:54:15 -0300

    Add files via upload

---
+++ BibliotecaLista/BibliotecaLista/ListaFuncoes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaLista
+{
+    public class ListaFuncoes
+    {
+        public static double Somar(List<double> lista)
+        {
+            double soma = 0;
+            foreach (double num in lista)
+            {
+                soma += num;
+            }
+            return soma;
+        }
+        public static double Media(List<double> lista)
+        {
+            double soma = Somar(lista);
+            return soma / lista.Count;
+        }
+
+    }
+}
